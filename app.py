91b6e75792bbdd67e773fabb9a66fefb10f0bad7
levickane <oneleggedlevi09@gmail.com>
2020-08-18 10:33:12 -0500

    cleaning product list
    can't figure out the price to cents thing....

---
+++ app.py
@@ -0,0 +1,76 @@
+from collections import OrderedDict
+import datetime
+import csv
+import os
+import sys
+import re
+
+from peewee import *
+
+db = SqliteDatabase('inventory.db')
+
+class Product(Model):
+    product_id = CharField(primary_key = True)
+    product_name = TextField(unique = True)
+    product_quantity = IntegerField(default = 0)
+    product_price = IntegerField(null = False)
+    date_updated = DateTimeField()
+    class Meta:
+        database = db
+
+def initialize():
+    """Creates the database and table"""
+    db.connect()
+    db.create_tables([Product], safe=True)
+    with open('inventory.csv', newline='') as inventory_csv_file:
+        inventory_reader = csv.DictReader(inventory_csv_file)
+        product_dicts = list(inventory_reader)
+        for product in product_dicts:
+            Product.create(
+                product_name = clean_name(product['product_name']),
+                product_price = price_to_cents(product['product_price']),
+                product_quantity = clean_quantity(product['product_quantity']),
+                date_updated = clean_date(product['date_updated'])
+            )
+            print(product_dicts)
+
+def clean_name(product):
+    return re.match("\"?\w+\s-\s\w+\,?\s?\w*\"?", product)
+
+def price_to_cents(product):
+    price_match = re.match("^\D\d*.\d{2}\Z", product)
+    if price_match:
+        price_no_sign = re.sub("\D", '', price_match)
+        price_in_cents = float(float(price_no_sign) * 100)
+        return price_in_cents
+
+def clean_quantity(product):
+    return int(product['product_quantity'])
+
+def clean_date(product):
+    return  datetime.datetime.strptime(product['date_updated'], '%m/%d/%Y')
+    
+
+def add_product():
+    pass
+
+def clear_terminal():
+    """Clears the terminal for a new entry"""
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def menu_loop():
+    pass
+
+def backup():
+    pass
+
+
+if __name__ == '__main__':
+    initialize()
+    
+    
+
+
+
+
+    
